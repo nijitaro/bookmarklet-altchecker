bcd5f8acdb0489d7ac57022ac43c5c4ffee367c5
Kotaro Imai <mail@hokypoky.info>
2009-11-16 23:50:15 +0900

    README修正、文字化け修正

--- altchecker.js
+++ altchecker.js
@@ -1,26 +1,29 @@
 javascript:function altChecker(){
-	a = document.getElementsByTagName('img');	var d = "";	var f = "";
-	for(i=0;i<a.length;i  ){
-		var b = a[i];		var fname = "　　" b.src.substring(b.src.lastIndexOf('/') 1,b.src.length);
-		b.title = b.alt   fname  " (" b.width "×" b.height ")";
+	a = document.getElementsByTagName('img');
+	var d = "";
+	var f = "";
+	for(i=0;i<a.length;i++){
+		var b = a[i];
+		var fname = "%E3%80%80%E3%80%80"+b.src.substring(b.src.lastIndexOf('/')+1,b.src.length);
+		b.title = b.alt + fname +" ("+b.width+"%C3%97"+b.height+")";
 		if( b.hasAttribute("alt") ) {
-			if ( /[ 　]$/.test(b.alt) ) {
+			if ( /[ %E3%80%80]$/.test(b.alt) ) {
 				b.style.outline='1px solid #00ff00';
-				f  = fname "\n";
-				e  ;
+				f += fname+"\n";
+				e++;
 			} else if ( b.alt == "" ) {
 				b.style.outline='1px solid #0099ff';
 			}
 		} else if ( b.alt == undefined || b.alt == "" ) {
 			b.style.outline='1px solid #ff0000';
-			d  = fname  "\n";
-			e  ;
+			d += fname +"\n";
+			e++;
 		}
 	}
 	if(e==0){
 		alert('Hi-Ho!!');
 	}else{
-		alert ('MISSING ALT!!\n' d '\nWRONG ALT!!\n' f);
+		alert ('MISSING ALT!!\n'+d+'\nWRONG ALT!!\n'+f);
 	}
 }
 var e=0;altChecker();
