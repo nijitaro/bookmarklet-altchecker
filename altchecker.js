6e97bf45b28c3dcde7ce3dea22e2f88adf882af7
Kotaro Imai <mail@hokypoky.info>
2009-11-16 23:38:41 +0900

    :)

---
+++ altchecker.js
@@ -0,0 +1,26 @@
+javascript:function altChecker(){
+	a = document.getElementsByTagName('img');	var d = "";	var f = "";
+	for(i=0;i<a.length;i  ){
+		var b = a[i];		var fname = "　　" b.src.substring(b.src.lastIndexOf('/') 1,b.src.length);
+		b.title = b.alt   fname  " (" b.width "×" b.height ")";
+		if( b.hasAttribute("alt") ) {
+			if ( /[ 　]$/.test(b.alt) ) {
+				b.style.outline='1px solid #00ff00';
+				f  = fname "\n";
+				e  ;
+			} else if ( b.alt == "" ) {
+				b.style.outline='1px solid #0099ff';
+			}
+		} else if ( b.alt == undefined || b.alt == "" ) {
+			b.style.outline='1px solid #ff0000';
+			d  = fname  "\n";
+			e  ;
+		}
+	}
+	if(e==0){
+		alert('Hi-Ho!!');
+	}else{
+		alert ('MISSING ALT!!\n' d '\nWRONG ALT!!\n' f);
+	}
+}
+var e=0;altChecker();
